52a5fbfc9f2b498b2205e39d3286c04d932d2fea
TUNA Caglayan <caglayan.tuna@irisa.fr>
2022-06-23 15:53:22 +0200

    udpate fucntions

--- tltorch/functional/__init__.py
+++ tltorch/functional/__init__.py
@@ -1,3 +1,3 @@
 from .convolution import convolve, tucker_conv
 from .linear import factorized_linear
-from ._stochastic_generalized_parafac import stochastic_generalized_parafac
+from ._stochastic_generalized_parafac import stochastic_generalized_parafac, stochastic_generalized_parafac_2
--- tltorch/functional/_stochastic_generalized_parafac.py
+++ tltorch/functional/_stochastic_generalized_parafac.py
@@ -8,6 +8,7 @@ from tensorly.random import random_cp
 from tensorly.cp_tensor import CPTensor, validate_cp_rank
 from tensorly.decomposition._cp import sample_khatri_rao
 from ..utils import loss_operator, gradient_operator
+import math
 
 
 def initialize_generalized_parafac(tensor, rank, init='random', svd='numpy_svd', non_negative=False, random_state=None):
@@ -253,7 +254,10 @@ def loss_operator_func(tensor, rank, loss, mask=None):
     epsilon = 1e-8
 
     if loss == 'gaussian':
-        return lambda x: tl.sum((tensor - vectorized_factors_to_tensor(x, shape, rank, mask)) ** 2) / size
+        def func(x):
+            est = vectorized_factors_to_tensor(x, shape, rank, mask)
+            return tl.sum((tensor - est) ** 2) / size
+        return func
     elif loss == 'bernoulli_odds':
         def func(x):
             est = vectorized_factors_to_tensor(x, shape, rank, mask)
@@ -390,6 +394,153 @@ def stochastic_generalized_parafac(tensor, rank, n_iter_max=1000, init='random',
     cp_tensor = CPTensor((None, factors))
     if return_errors:
         return cp_tensor, error
+    else:
+        return cp_tensor
+
+def stochastic_generalized_parafac_2(tensor, rank, n_iter_max=1000, init='random', return_errors=False,
+                                   loss='gaussian', epochs=20, batch_size=200, lr=0.01, beta_1=0.9, beta_2=0.999,
+                                   mask=None, random_state=None):
+    """ Generalized PARAFAC decomposition by using ADAM optimization.
+    Computes a rank-`rank` decomposition of `tensor` [1]_ such that::
+
+        tensor ~ D([|weights; factors[0], ..., factors[-1] |])
+
+    where D is a parametric distribution such as Gaussian, Poisson, Rayleigh, Gamma or Bernoulli.
+
+    Generalized parafac essentially performs the same kind of decomposition as the parafac function,
+    but using a more diverse set of user-chosen loss functions. Under the hood, it relies on stochastic
+    optimization using a home-made implementation of ADAM.
+
+    Parameters
+    ----------
+    tensor : ndarray
+    rank  : int
+        Number of components.
+    n_iter_max : int
+        Maximum number of iteration
+    init : {'random', CPTensor}, optional
+        Type of factor matrix initialization.
+        If a CPTensor is passed, this is directly used for initialization.
+        See `initialize_factors`.
+    return_errors : bool, optional
+        Activate return of iteration errors
+    loss : {'gaussian', 'bernoulli_odds', 'bernoulli_logit', 'rayleigh', 'poisson_count', 'poisson_log', 'gamma'}
+        Default : 'gaussian'
+    epochs : int
+        Default : 20
+    batch_size : int
+        Default : 200
+    lr : float
+        Default : 0.01
+    beta_1 : float
+        ADAM optimization parameter.
+        Default : 0.9
+    beta_2 : float
+        ADAM optimization parameter.
+        Default : 0.999
+    mask : ndarray
+        array of booleans with the same shape as ``tensor`` should be 0 where
+        the values are missing and 1 everywhere else.
+    random_state : {None, int, np.random.RandomState}
+
+    Returns
+    -------
+    CPTensor : (weight, factors)
+        * weights : 1D array of shape (rank, )
+          * all ones if normalize_factors is False (default)
+          * weights of the (normalized) factors otherwise
+        * factors : List of factors of the CP decomposition element `i` is of shape ``(tensor.shape[i], rank)``
+    errors : list
+        A list of reconstruction errors at each iteration of the algorithms.
+
+    References
+    ----------
+    .. [1] Hong, D., Kolda, T. G., & Duersch, J. A. (2020).
+           Generalized canonical polyadic tensor decomposition. SIAM Review, 62(1), 133-163.
+    .. [2] Kolda, T. G., & Hong, D. (2020). Stochastic gradients for large-scale tensor decomposition.
+           SIAM Journal on Mathematics of Data Science, 2(4), 1066-1095.
+    """
+    rank = validate_cp_rank(tl.shape(tensor), rank=rank)
+    rng = tl.check_random_state(random_state)
+    rec_errors = []
+    modes = [mode for mode in range(tl.ndim(tensor))]
+    # initial tensor
+    if loss == 'gamma' or loss == 'rayleigh' or loss == 'poisson_count' or loss == 'bernoulli_odds':
+        non_negative = True
+    else:
+        non_negative = False
+
+    # initial tensor
+    _, factors = initialize_generalized_parafac(tensor, rank, init=init, non_negative=non_negative, random_state=rng)
+    # parameters for ADAM optimization
+    momentum_first = []
+    momentum_second = []
+    t_iter = 1
+    indices_tuple = tuple([rng.randint(0, tl.shape(f)[0], size=batch_size, dtype=int) for f in factors])
+    if mask is not None:
+        current_loss = loss_operator(tensor[indices_tuple],
+                                     tl.sum(sample_khatri_rao(factors, indices_list=indices_tuple, n_samples=batch_size)[0],
+                                            axis=1), loss=loss, mask=mask[indices_tuple])
+    else:
+        current_loss = loss_operator(tensor[indices_tuple],
+                                     tl.sum(sample_khatri_rao(factors, indices_list=indices_tuple, n_samples=batch_size)[0],
+                                            axis=1), loss=loss)
+    # global loss
+    current_loss = tl.sum(current_loss)
+    for i in modes:
+        momentum_first.append(tl.zeros(tl.shape(factors[i])))
+        momentum_second.append(tl.zeros(tl.shape(factors[i])))
+    epsilon = 1e-8
+    bad_epochs = 0
+    max_bad_epochs = 20
+    for epoch in range(epochs):
+        loss_old = tl.copy(current_loss)
+        factors_old = [tl.copy(f) for f in factors]
+        momentum_first_old = [tl.copy(f) for f in momentum_first]
+        momentum_second_old = [tl.copy(f) for f in momentum_second]
+        for iteration in range(n_iter_max):
+            gradient = stochastic_gradient(tensor, factors, batch_size, random_state=rng, loss=loss)
+            for mode in modes:
+                # adam optimization
+                momentum_first[mode] = (beta_1 * momentum_first[mode]) + (1 - beta_1) * gradient[mode]
+                momentum_second[mode] = beta_2 * momentum_second[mode] + (1 - beta_2) * (gradient[mode] ** 2)
+                momentum_first_hat = momentum_first[mode] / (1 - (beta_1 ** t_iter))
+                momentum_second_hat = momentum_second[mode] / (1 - (beta_2 ** t_iter))
+                factors[mode] = factors[mode] - lr * momentum_first_hat / (tl.sqrt(momentum_second_hat) + epsilon)
+
+                if loss == 'gamma' or loss == 'rayleigh' or loss == 'poisson_count' or loss == 'bernoulli_odds':
+                    factors[mode] = tl.clip(factors[mode], 0)
+
+            t_iter += 1
+        # Compute the current error
+        if mask is not None:
+            current_loss = loss_operator(tensor[indices_tuple],
+                                         tl.sum(sample_khatri_rao(factors, indices_list=indices_tuple, n_samples=batch_size)[0],
+                                         axis=1), loss=loss, mask=mask[indices_tuple])
+        else:
+            current_loss = loss_operator(tensor[indices_tuple],
+                                         tl.sum(sample_khatri_rao(factors, indices_list=indices_tuple, n_samples=batch_size)[0],
+                                         axis=1), loss=loss)
+        # global loss
+        current_loss = tl.sum(current_loss)
+        if current_loss >= loss_old:
+            lr = lr / 10
+            factors = [tl.copy(f) for f in factors_old]
+            current_loss = tl.copy(loss_old)
+            t_iter -= iteration
+            momentum_first = [tl.copy(f) for f in momentum_first_old]
+            momentum_second = [tl.copy(f) for f in momentum_second_old]
+            bad_epochs += 1
+        else:
+            bad_epochs = 0
+        rec_error = current_loss / tl.norm(tensor)
+        rec_errors.append(rec_error)
+        if bad_epochs >= max_bad_epochs:
+            print("Sufficient number of bad epochs")
+            break
+    cp_tensor = CPTensor((None, factors))
+    if return_errors:
+        return cp_tensor, rec_errors
     else:
         return cp_tensor
 
